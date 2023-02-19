1b0ba964c11244919008a5951332d6a83b068ea6
Sebastian Kielbasa <sebastiankielbasa101@gmail.com>
2023-02-19 22:59:27 +0100

    Erste App_Fertig

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,42 +16,47 @@ class HomeScreen extends StatelessWidget {
           color: Colors.white ,
           width: 350,
           height: 200,
-          child: Column(
-            children: [
-              Row(
-                 children: [
-                   const Icon(
-                     Icons.account_circle,
-                     size:100,
-                   ),
-                   Column(
-                     crossAxisAlignment: CrossAxisAlignment.start ,
-                     children: const [
-                       Text('Max Mustermann', textScaleFactor: 2,),
-                       Text('Flutter Enwickler', textScaleFactor: 1.2,),
-                     ],
-                   ),
-                 ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text('123 Musterstraße'),
-                  Text('06605833825'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.accessibility, size:35),
-                  Icon(Icons.timer, size:35),
-                  Icon(Icons.phone_android, size:35),
-                  Icon(Icons.phone_iphone, size:35),
-                ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              children: [
+                Row(
+                   children: [
+                     const Icon(
+                       Icons.account_circle,
+                       size:100,
+                     ),
+                     Column(
+                       crossAxisAlignment: CrossAxisAlignment.start ,
+                       children: const [
+                         Text('Max Mustermann', textScaleFactor: 2,),
+                         Text('Flutter Enwickler', textScaleFactor: 1.2,),
+                       ],
+                     ),
+                   ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text('123 Musterstraße'),
+                    Text('06605833825'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.accessibility, size:35),
+                    Icon(Icons.timer, size:35),
+                    Icon(Icons.phone_android, size:35),
+                    Icon(Icons.phone_iphone, size:35),
+                  ],
 
 
-              ),
-            ],
+                ),
+              ],
+            ),
           )
         ),
       ),
