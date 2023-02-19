6f0ecc0ad55d42965938123683b375b76cac6b8d
Sebastian Kielbasa <sebastiankielbasa101@gmail.com>
2023-02-19 22:55:22 +0100

    Erste App

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,41 +6,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text('Meine App'),
+        title: const Text('Meine App'),
+        backgroundColor: Colors.green,
       ),
-      body: Container(
-        color: Colors.red,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              color: Colors.green,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("HeyHoxxxxxxxxxx  "),
-                  Text("HeyHoxxxxxxxxxx123213"),
+      body: Center(
+        child: Container(
+          color: Colors.white ,
+          width: 350,
+          height: 200,
+          child: Column(
+            children: [
+              Row(
+                 children: [
+                   const Icon(
+                     Icons.account_circle,
+                     size:100,
+                   ),
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.start ,
+                     children: const [
+                       Text('Max Mustermann', textScaleFactor: 2,),
+                       Text('Flutter Enwickler', textScaleFactor: 1.2,),
+                     ],
+                   ),
+                 ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('123 Musterstraße'),
+                  Text('06605833825'),
                 ],
               ),
-            ),
-            Container(
-              color: Colors.yellow,
-              child: Row(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("HeyHo"),
+                  Icon(Icons.accessibility, size:35),
+                  Icon(Icons.timer, size:35),
+                  Icon(Icons.phone_android, size:35),
+                  Icon(Icons.phone_iphone, size:35),
                 ],
-              ),
-            ),
-            Container(
-              child: Text("ABCDE"),
-              color: Colors.blue
 
-            )
-          ],
+
+              ),
+            ],
+          )
         ),
-      )
+      ),
     );
   }
 }
