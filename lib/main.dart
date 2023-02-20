893c02fc3cfd2556c107bd271516f5e371b97eab
Sebastian Kielbasa <sebastiankielbasa101@gmail.com>
2023-02-20 14:02:42 +0100

    Assets_App4

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firstapp/screens/my_app.dart';
 //First Method Call MyApp (Start of the APP), see my_app.dart
 void main() {
   runApp(MyApp());
+
 }
 
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,66 +4,72 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
 
 
-final List<Widget> exampleList =[
-  Container(height: 20, width:20, color: Colors.green[50]),
-  Container(height: 20, width:20, color: Colors.green[100]),
-  Container(height: 20, width:20, color: Colors.green[200]),
-  Container(height: 20, width:20, color: Colors.green[300]),
-  Container(height: 20, width:20, color: Colors.green[400]),
-  Container(height: 20, width:20, color: Colors.green[500]),
-  Container(height: 20, width:20, color: Colors.green[600]),
-  Container(height: 20, width:20, color: Colors.green[700]),
-  Container(height: 20, width:20, color: Colors.green[800]),
-  Container(height: 20, width:20, color: Colors.green[900]),
-  Container(height: 20, width:20, color: Colors.blue[50]),
-  Container(height: 20, width:20, color: Colors.blue[100]),
-  Container(height: 20, width:20, color: Colors.blue[200]),
-  Container(height: 20, width:20, color: Colors.blue[300]),
-  Container(height: 20, width:20, color: Colors.blue[400]),
-  Container(height: 20, width:20, color: Colors.blue[500]),
-  Container(height: 20, width:20, color: Colors.blue[600]),
-  Container(height: 20, width:20, color: Colors.blue[700]),
-  Container(height: 20, width:20, color: Colors.blue[800]),
-  Container(height: 20, width:20, color: Colors.blue[900]),
-];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[400],
+        backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Meine App'),
-        backgroundColor: Colors.green,
+        title: const Text('Jobsuchender'),
+        backgroundColor: Colors.black,
       ),
         body: Center(
-          child: Container(
-            width: 350,
-           height: 600,
-           color: Colors.green,
-            child: Stack(
-              alignment: Alignment.bottomLeft,
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.red,
-                ),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-
-                )
-
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyCircularAvatar(),
+              const SizedBox(height: 16),
+              MyCustomeText(),
+              MyButton(),
+            ],
           ),
-        )
+        ),
     );
   }
 }
 
+class MyCircularAvatar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("images/sebastian.JPG"), fit: BoxFit.fitHeight),
+        border: Border.all(color: Colors.black,width: 8),
+        borderRadius: BorderRadius.circular(200),
+      ),
+    );
+  }
+}
+
+
+class MyCustomeText extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+        "Sebastian Kielbasa",
+      style: TextStyle(
+        fontSize: 42.0,
+        fontFamily: "Ubuntu"
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {  },
+        child: Text("Klick mich"),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.black,
+          elevation: 0,
+        ),
+      ),
+    );
+  }
+}
